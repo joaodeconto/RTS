28b9b9480080a52c3d15d5f0897fafb88d5caff4
Cristian Ritter <cristiansrguitar@hotmail.com>
2013-02-28 17:18:57 -0300

    Fix: Unidade com IA um pouco mais aprimorada
    Signed-off-by: Cristian Ritter <cristiansrguitar@hotmail.com>

--- Assets/Project Assets/_Scripts/NGUI/UnitCallbackButton.cs
+++ Assets/Project Assets/_Scripts/NGUI/UnitCallbackButton.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class UnitCallbackButton : MonoBehaviour
 {
 	public Unit unit;
-	public float timeToCreate;
 	public FactoryBase factory;
 	
 	public void Init (Unit unit, FactoryBase factory)
