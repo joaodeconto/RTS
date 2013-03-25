673c088fea6cb777eef42d1c62d8e2422e4909bc
Matheus-de-Souza <mws.souza@gmail.com>
2013-03-25 13:57:56 -0300

    Close #16 Add: EventManager e other events

--- Assets/Project Assets/_Scripts/Controller/GameplayManager.cs
+++ Assets/Project Assets/_Scripts/Controller/GameplayManager.cs
@@ -13,23 +13,24 @@ public class Team
 
 public class GameplayManager : MonoBehaviour
 {
+	public const int MAX_POPULATION_ALLOWED = 200;
 	public const int NUMBER_INCREMENT_AND_DECREMENT_UNITS = 5;
-	
+
 	public Team[] teams;
-	
-	public int numberOfUnits { get; protected set; } 
+
+	public int numberOfUnits { get; protected set; }
 	public int maxOfUnits { get; protected set; }
-	
+
 	protected Dictionary<int, int> teamNumberOfStats = new Dictionary<int, int>();
 	protected int loserTeams;
 	protected bool loseGame = false;
 	protected bool winGame = false;
-	
+
 	public int MyTeam {get; protected set;}
-	
+
 	// Resources
 	public ResourcesManager resources;
-	
+
 	public void Init ()
 	{
 		if (!PhotonNetwork.offlineMode)
@@ -40,7 +41,7 @@ public class GameplayManager : MonoBehaviour
 		{
 			MyTeam = 0;
 		}
-		
+
 		for (int i = 0; i != teams.Length; i++)
 		{
 			if (MyTeam == i)
@@ -48,10 +49,10 @@ public class GameplayManager : MonoBehaviour
 				Camera.mainCamera.transform.position = teams[i].initialPosition.position;
 			}
 		}
-		
+
 		IncrementMaxOfUnits ();
 	}
-	
+
 	public Color GetColorTeam (int teamID)
 	{
 		if (teamID >= 0 && teamID < teams.Length) return teams[teamID].color;
@@ -61,47 +62,57 @@ public class GameplayManager : MonoBehaviour
 			return Color.black;
 		}
 	}
-	
+
 	public bool IsSameTeam (int teamID)
 	{
 		return teamID == MyTeam;
 	}
-	
+
 	public bool IsSameTeam (Unit soldier)
 	{
 		return soldier.Team == MyTeam;
 	}
-	
+
 	public bool IsSameTeam (FactoryBase factory)
 	{
 		return factory.Team == MyTeam;
 	}
-	
+
 	public void IncrementUnit (int teamID)
 	{
 		if (IsSameTeam (teamID)) ++numberOfUnits;
 	}
-	
+
 	public void DecrementUnit (int teamID)
 	{
 		if (IsSameTeam (teamID)) --numberOfUnits;
 	}
-	
+
 	public void IncrementMaxOfUnits ()
 	{
 		maxOfUnits += NUMBER_INCREMENT_AND_DECREMENT_UNITS;
 	}
-	
+
 	public void DecrementMaxOfUnits ()
 	{
 		maxOfUnits -= NUMBER_INCREMENT_AND_DECREMENT_UNITS;
 	}
-	
-	public bool IsLimitMaxUnits ()
+
+	public bool NeedMoreHouses
 	{
-		return numberOfUnits >= maxOfUnits;
+		get
+		{
+			return (numberOfUnits >= maxOfUnits);
+		}
 	}
-	
+
+	public bool ReachedMaxPopulation
+	{
+		get
+		{
+			return (maxOfUnits >= MAX_POPULATION_ALLOWED);
+		}
+	}
 	public void AddStatTeamID (int teamID)
 	{
 		if (teamNumberOfStats.ContainsKey(teamID))
@@ -109,17 +120,17 @@ public class GameplayManager : MonoBehaviour
 		else
 			teamNumberOfStats.Add (teamID, 1);
 	}
-	
+
 	public void RemoveStatTeamID (int teamID)
 	{
 		if (teamNumberOfStats.ContainsKey(teamID))
 		    teamNumberOfStats[teamID] = teamNumberOfStats[teamID] - 1;
 		else
 			return;
-		
+
 		CheckCondition (teamID);
 	}
-	
+
 	public void RemoveAllStats (int teamID)
 	{
 		if (teamNumberOfStats.ContainsKey(teamID))
@@ -135,10 +146,10 @@ public class GameplayManager : MonoBehaviour
 		}
 		else
 			return;
-		
+
 		CheckCondition (teamID);
 	}
-	
+
 	public void CheckCondition (int teamID)
 	{
 		if (teamNumberOfStats.ContainsKey(teamID))
@@ -159,25 +170,25 @@ public class GameplayManager : MonoBehaviour
 		{
 			return;
 		}
-		
+
 		if (loserTeams == teamNumberOfStats.Count-1
 			&& !loseGame)
 		{
 			winGame = true;
 		}
-		
+
 	}
-	
+
 	// TODO: Mostrando só os valores na tela
 	void OnGUI ()
 	{
 		GUI.Box (new Rect(10, 10, 150, 25), "Resources: " + resources.NumberOfRocks.ToString ());
 		GUI.Box (new Rect(10, 35, 150, 25), "Units: " + numberOfUnits.ToString () + "/" + maxOfUnits.ToString ());
-		
-		if (loseGame) GUI.Box (new Rect(Screen.width/2 - 75, Screen.height/2 - 12, 150, 25), "LOSER! ):"); 
+
+		if (loseGame) GUI.Box (new Rect(Screen.width/2 - 75, Screen.height/2 - 12, 150, 25), "LOSER! ):");
 		else if (winGame) GUI.Box (new Rect(Screen.width/2 - 75, Screen.height/2 - 12, 150, 25), "WIN! :D");
 	}
-	
+
 	void EndGame ()
 	{
 		Visiorama.ComponentGetter.Get<NetworkManager>().photonView.RPC ("ChangeLevel", PhotonTargets.All, 0);
--- Assets/Project Assets/_Scripts/Factory/FactoryBase.cs
+++ Assets/Project Assets/_Scripts/Factory/FactoryBase.cs
@@ -29,9 +29,9 @@ public class FactoryBase : IStats
 
 	public enum BuildingState
 	{
-		Base = 0,
+		Base       = 0,
 		Unfinished = 1,
-		Finished = 2
+		Finished   = 2
 	}
 
 	public UnitFactory[] unitsToCreate;
@@ -66,10 +66,12 @@ public class FactoryBase : IStats
 	protected UISlider buildingSlider;
 
 	public bool wasVisible = false;
+	public bool alreadyCheckedMaxPopulation = false;
 
 	public bool IsNeededRepair
 	{
-		get {
+		get
+		{
 			return Health != MaxHealth;
 		}
 	}
@@ -122,9 +124,20 @@ public class FactoryBase : IStats
 	{
 		SyncAnimation ();
 
-		if (!wasBuilt ||
-			listedToCreate.Count == 0 ||
-			gameplayManager.IsLimitMaxUnits ()) return;
+		if (!wasBuilt || listedToCreate.Count == 0)
+			return;
+
+		if (gameplayManager.ReachedMaxPopulation)
+		{
+			if (!alreadyCheckedMaxPopulation)
+			{
+				alreadyCheckedMaxPopulation = true;
+				eventManager.AddEvent("reach max population");
+			}
+			return;
+		}
+		else
+			alreadyCheckedMaxPopulation = false;
 
 		if (unitToCreate == null)
 		{
@@ -207,9 +220,7 @@ public class FactoryBase : IStats
 			unitName = unit.name;
 		}
 
-		EventManager.Event e = GetEvent("create unit");
-
-		eventManager.AddEvent(string.Format(e.Message, unitName), unit.guiTextureName);
+		eventManager.AddEvent("create unit", unitName, unit.guiTextureName);
 
 		if (!hasWaypoint) return;
 
@@ -284,6 +295,8 @@ public class FactoryBase : IStats
 			if (!wasBuilt)
 			{
 				wasBuilt = true;
+
+				eventManager.AddEvent("building finish", this.name, this.guiTextureName);
 				SendMessage ("ConstructFinished", SendMessageOptions.DontRequireReceiver);
 			}
 			return false;
@@ -352,7 +365,8 @@ public class FactoryBase : IStats
 															if (!factory.OverLimitCreateUnit)
 																factory.EnqueueUnitToCreate (unit);
 															else
-																;//TODO enviar mensagem
+																//TODO enviar mensagem
+																eventManager.AddEvent("reach enqueued units");
 														});
 			}
 
@@ -422,7 +436,6 @@ public class FactoryBase : IStats
 			ht["unit"] = unit;
 			ht["name"] = "button-" + Time.time;
 
-			//TODO colocar mais coisas aqui
 			hudController.CreateEnqueuedButtonInInspector ( (string)ht["name"],
 															FactoryBase.FactoryQueueName,
 															ht,
@@ -434,8 +447,7 @@ public class FactoryBase : IStats
 															});
 		}
 		else
-			;//TODO mensagem para o usuário saber
-			 //que não possui recursos suficientes para criar tal unidade
+			eventManager.AddEvent("out of founds", unit.name);
 	}
 
 	private void DequeueUnit(Hashtable ht)
