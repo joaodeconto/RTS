90344c21e205b798c094d7ff52fcdb54cc5525f5
Joao De Conto <joaodeconto@hotmail.com>
2014-05-19 17:38:29 -0300

    FB.feed test;
    Signed-off-by: Joao De Conto <joaodeconto@hotmail.com>

--- Assets/_Scripts/NGUI/EndGameHUD.cs
+++ Assets/_Scripts/NGUI/EndGameHUD.cs
@@ -38,5 +38,28 @@ public class EndGameHUD : MonoBehaviour {
 		{
 			gameObject.SetActive (false);
 		});
+
+		option = transform.FindChild ("Victory").
+			transform.FindChild ("Facebook Win1").gameObject;
+		
+		defaultCallbackButton = option.AddComponent<DefaultCallbackButton> ();
+		defaultCallbackButton.Init (null,
+		                            (ht_dcb) =>
+		                            {
+			if (FB.IsLoggedIn)
+			{
+
+			FB.Feed(
+					link: "https://www.facebook.com/RexTribalS",
+					linkName: "Victory!",
+					linkCaption: " 'Almighty leader, the enemy was defeated!'",
+					linkDescription: " 'Let us prepare the ritual, gather the gold for the blessing of the gods!..'", 
+					picture: "https://www.facebook.com/photo.php?fbid=181734578645845&set=pb.181504285335541.-2207520000.1400530680.&type=3&theater"
+		
+				
+				);
+			}
+			
+		});
 	}
 }
